d32bf498cdf5fa1a1d1091067e1f904a0fc41c91
Kashan Ali <kali26@illinois.edu>
2025-01-14 23:39:06 -0500

    Task 4 Started

--- server/database/app.js
+++ server/database/app.js
@@ -78,7 +78,7 @@ app.get('/fetchDealers/:state', async (req, res) => {
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
   try {
-    const document = await Dealerships.find({ id: dealerId }); // Use _id for MongoDB ID
+    const documents = await Dealerships.find({ id: req.params.id }); // Use _id for MongoDB ID
     res.json(documents);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching documents' });
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -7,15 +7,17 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
 from django.contrib import messages
 from datetime import datetime
+from django.views.decorators.csrf import csrf_exempt
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
 import json
-from django.views.decorators.csrf import csrf_exempt
 from .models import CarMake, CarModel
-from .populate import initiate
 from .restapis import get_request, analyze_review_sentiments, post_review
+from django.views.decorators.csrf import csrf_exempt
+# from .populate import initiate
+
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -40,9 +42,15 @@ def login_user(request):
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
+# def logout_request(request)
+#         logout(request)
+#     data = {"userName":""}
+#     return JsonResponse(data)
+# # ...
+
 def logout_request(request):
     logout(request)
-    data = {"userName":""}
+    data = {"userName": ""}
     return JsonResponse(data)
 
 # Create a `registration` view to handle sign up request
@@ -78,15 +86,6 @@ def registration(request):
         data = {"userName":username,"error":"Already Registered"}
         return JsonResponse(data)
 
-#Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
-def get_dealerships(request, state="All"):
-    if(state == "All"):
-        endpoint = "/fetchDealers"
-    else:
-        endpoint = "/fetchDealers/"+state
-    dealerships = get_request(endpoint)
-    return JsonResponse({"status":200,"dealers":dealerships})
-
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
@@ -121,8 +120,7 @@ def add_review(request):
             return JsonResponse({"status":401,"message":"Error in posting review"})
     else:
         return JsonResponse({"status":403,"message":"Unauthorized"})
-
-
+# ...
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
@@ -134,3 +132,12 @@ def get_cars(request):
     for car_model in car_models:
         cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels":cars})
+
+#Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
+def get_dealerships(request, state="All"):
+    if(state == "All"):
+        endpoint = "/fetchDealers"
+    else:
+        endpoint = "/fetchDealers/"+state
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status":200,"dealers":dealerships})
