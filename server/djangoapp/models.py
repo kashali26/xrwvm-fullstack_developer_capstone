b8cd1b31c62496eed0118bddfdd922c2184fa64f
Kashan Ali <kali26@illinois.edu>
2025-01-16 11:28:00 -0500

    bug fixxes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -10,6 +10,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    country_of_origin = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name  
@@ -21,17 +22,15 @@ class CarModel(models.Model):
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
-       
-    ]
+        ('WAGON', 'WAGON'),
+        ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(
         default=2023,
         validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
-        ]
-    )
+        ])
 
     def __str__(self):
         return self.name  
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -9,30 +9,30 @@ backend_url = os.getenv(
     'backend_url', default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    default="http://localhost:5050")
 
+
+# Create rest endpoints
 
 def get_request(endpoint, **kwargs):
     params = ""
     if (kwargs):
         for key, value in kwargs.items():
-            params = params+key+"="+value+"&"
+            params = params + key + "=" + value + "&"
 
-    request_url = backend_url+endpoint+"?"+params
+    request_url = backend_url + endpoint + "?" + params
 
     print("GET from {} ".format(request_url))
     try:
-        # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except Exception:
-        # If any error occurs
-        print("Network exception occurred")
+    except Exception as e:
+        print(f"Error: {e}")
+
 
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url + "analyze/" + text
     try:
-        # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
     except Exception as err:
@@ -41,10 +41,10 @@ def analyze_review_sentiments(text):
 
 
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
         response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except Exception:
-        print("Network exception occurred")
+    except Exception as e:
+        print(f"Error: {e}")
