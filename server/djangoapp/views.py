57be20adc857c5261d9c9d51fb7b69f7788178fc
Kashan Ali <kali26@illinois.edu>
2025-05-19 01:37:36 -0400

    changes

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -78,7 +78,7 @@ def get_dealerships(request, state="All"):
     else:
         endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
-    return JsonResponse({"status":200,"dealers":dealerships})
+    return JsonResponse({"status": 200, "dealers": dealerships})
 
 def get_dealer_reviews(request, dealer_id):
     if (dealer_id):
@@ -107,11 +107,11 @@ def add_review(request):
         data = json.loads(request.body)
         try:
             response = post_review(data)
-            return JsonResponse({"status":200})
+            return JsonResponse({"status": 200})
         except:
-            return JsonResponse({"status":401,"message":"Error in posting review"})
+            return JsonResponse({"status": 401, "message":"Error in posting review"})
     else:
-        return JsonResponse({"status":403,"message":"Unauthorized"})
+        return JsonResponse({"status": 403, "message":"Unauthorized"})
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
