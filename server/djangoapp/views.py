37de27d5904767efa3a7d1c6cf5dbaef29dc925f
Kashan Ali <kali26@illinois.edu>
2025-05-20 15:16:26 -0400

    fixed views

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,11 +1,7 @@
 # Uncomment the required imports before adding the code
 
-from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -51,7 +47,6 @@ def logout_request(request):
 @csrf_exempt
 def registration(request):
     context = {}
-    
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
